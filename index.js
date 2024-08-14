1b8a2a01f2f569655dc53c4314bf6b3d35fc8860
aykutkaraca <karaca.aykut@gmail.com>
2024-08-14 16:25:09 +0300

    removed alert boxes

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ window.onload = async () => {
     getOS();
     /* let elem = document.getElementById('scratcher-box');
     let ht = window.getComputedStyle(elem, null).getPropertyValue("top"); */
-    let mql = window.matchMedia("(max-width: 320px)");
+    /* let mql = window.matchMedia("(max-width: 320px)");
     alert(mql.matches);
-    alert(window.innerWidth);
+    alert(window.innerWidth); */
     
     var a = new Image();
     /* if (userOS ="iOS" && userOSver<13) {
